bcf8b42592f1931fde2c379f8422e4d195591512
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2025-03-27 14:17:11 +0000

    Item Collection updated.

--- portalapi/data/item.py
+++ portalapi/data/item.py
@@ -1,9 +1,9 @@
 class Item:
-    def __init__(self, id, customer, salesorder):
+    def __init__(self, id, customer, sales_order):
         self.id = id
         self.customer = customer
-        self.salesorder = salesorder
+        self.sales_order = sales_order
 
     @classmethod
     def from_mongodb(cls, list):
-        return cls(list['_id'], list['Customer'], list['salesorder'])
+        return cls(list['_id'], list['Customer'], list['Sales_Order'])
