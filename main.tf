3dbc8cf4fb93f10d97d143d8bd878b2ba8798cd0
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2025-04-11 12:24:02 +0100

    Add Terraform to CICD

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
             version = "~>4.0"
         }
     }
+    backend "azurerm" {
+        resource_group_name  = "PortalApi"
+        storage_account_name = "portalapistorageacc"
+        container_name       = "portalapiblob"
+        key                  = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
