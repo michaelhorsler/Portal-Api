2a7f80a9782901f7b4eab4ee753a3bad6e63e10e
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2025-04-14 12:20:18 +0100

    Add Terraform variables to Github Secrets

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
     subscription_id = "50ef3721-085f-48dd-a4f1-d17b05980663"
     tenant_id       = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4"
     client_id       = "70bd1492-7521-4c03-bdaf-2f7be682acac"
-    client_secret   = SERVICE_PRINCIPLE_CLIENT_SECRET
+    client_secret   = var.SERVICE_PRINCIPLE_CLIENT_SECRET
 }
 data "azurerm_resource_group" "main" {
     name     = "PortalApi"
@@ -48,17 +48,17 @@ resource "azurerm_linux_web_app" "main" {
         application_stack {
             docker_image_name = "portalapi:latest"
             docker_registry_url = "https://portalapicontainer.azurecr.io"  
-            docker_registry_username = DOCKER_SERVER_USR
-            docker_registry_password = DOCKER_SERVER_PWD
+            docker_registry_username = var.DOCKER_SERVER_USR
+            docker_registry_password = var.DOCKER_SERVER_PWD
         }
     }
     app_settings = {
-        "FLASK_APP" = FLASK_APP
-        "FLASK_DEBUG" = FLASK_DEBUG
-        "MONGODBASE_CONN_STRING" = MONGODBASE_CONN_STRING
-        "MONGODBASE" = MONGODBASE
-        "SECRET_KEY" = SECRET_KEY
-        "WEBSITES_PORT" = WEBSITES_PORT
+        "FLASK_APP" = var.FLASK_APP
+        "FLASK_DEBUG" = var.FLASK_DEBUG
+        "MONGODBASE_CONN_STRING" = var.MONGODBASE_CONN_STRING
+        "MONGODBASE" = var.MONGODBASE
+        "SECRET_KEY" = var.SECRET_KEY
+        "WEBSITES_PORT" = var.WEBSITES_PORT
         "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = false
     }
 }
